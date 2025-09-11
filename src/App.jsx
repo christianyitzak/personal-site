338e75bc1f26e50bc1035fa5c59d1ac6f136e7b2
christianyitzak <christianyitzak@gmail.com>
2025-09-11 14:07:06 +0700

    make the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
 function App() {
     return (
         <Routes>
             <Route index element={<HomePage />} />
+            <Route path="about" element={<Header />} />
+            <Route path="blog" element={<Header />} />
+            <Route path="projects" element={<Header />} />
         </Routes>
     );
 }
